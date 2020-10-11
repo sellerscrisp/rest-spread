331f979a2d138545ed439111d3a2858ef2195ee3
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-11 10:50:50 -0500

    exercise code works

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*
+/**
 
 Refactor using rest operator and arrow function
 
@@ -19,4 +19,37 @@ const findMin = (...arguments) => Math.min(...arguments)
 // mergeObjects
 const mergeObjects = (object1, object2) => ({ ...object1, ...object2 })
 
-//
+// doubleAndReturnArgs
+const doubleAndReturnArgs = (arr, ...arguments) => [
+  ...arr,
+  ...arguments.map(num => num * 2)
+]
+
+// slice and dice
+
+/** remove a random element in the items array
+and return a new array without that item. */
+const removeRandom = items => {
+  let index = Math.floor(Math.random() * items.length)
+  return [...items.slice(0, index), ...items.slice(index + 1)]
+}
+
+/** Return a new array with every item in array1 and array2. */
+const extend = (array1, array2) => [...array1, ...array2]
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+const addKeyVal = (obj, key, val) => ({ ...obj, [key]: val })
+
+/** Return a new object with a key removed. */
+const removeKey = (obj, key) => {
+  let newObj = { ...obj }
+  delete newObj[key]
+  return newObj
+}
+
+/** Combine two objects and return a new object. */
+const combine = (obj1, obj2) => ({ ...obj1, ...obj2 })
+
+/** Return a new object with a modified key and value. */
+const update = (obj, key, val) => ({ ...obj, [key]: val })
